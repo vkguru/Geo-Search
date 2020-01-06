eb064e39a0f6c19c2f71853d19a6ea72e03a3d3f
vkguru <30696225+vkguru@users.noreply.github.com>
2020-01-06 18:28:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,25 @@ const weatherApi = () => {
     });  
 }
 
-const sbtn = document.querySelector('#sbtn');
+/*const sbtn = document.querySelector('#sbtn');
 sbtn.addEventListener('click', weatherApi);
+
+const sbtn = document.querySelector('#sbtn');
+
+
+const weatherApi = () => {
+    const key = '4d59a213650edb3bccb1ef7cf2f7c6dd';
+    const cityName = document.querySelector('#search');
+    const city = ''; 
+    city.appendChild(cityName);
+
+    fetch('api.openweathermap.org/data/2.5/weather?q=' + cityName+ '&appid=' + key)
+    .then((resp) => {return resp.json()})
+    .then((data) => {console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });  
+}
+
+window.addEventListener('click', weatherApi);*/
